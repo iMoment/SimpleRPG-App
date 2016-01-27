6aeb50186fa5d7ab43fe6ca797ae52d4acf831bf
Stanley Pan <stanleypan.egre@gmail.com>
2016-01-27 21:10:52 +0900

    Add Class Kimara

---
+++ SimpleRPG/Kimara.swift
@@ -0,0 +1,30 @@
+//
+//  Kimara.swift
+//  SimpleRPG
+//
+//  Created by Stanley Pan on 1/27/16.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+import Foundation
+
+class Kimara: Enemy {
+    
+    let IMMUNE_MAX = 15
+    
+    override var loot: [String] {
+        return ["Tough Hide", "Kimara Venom", "Rare Trident"]
+    }
+    
+    override var type: String {
+        return "Kimara"
+    }
+    
+    override func attemptAttack(attackPwr: Int) -> Bool {
+        if attackPwr >= IMMUNE_MAX {
+            return super.attemptAttack(attackPwr)
+        } else {
+            return false
+        }
+    }
+}
