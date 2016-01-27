31afa618fde58ef80dc5ee504b4df02560983252
Stanley Pan <stanleypan.egre@gmail.com>
2016-01-27 20:51:16 +0900

    Add Class Player

--- SimpleRPG/Character.swift
+++ SimpleRPG/Character.swift
@@ -25,5 +25,24 @@ class Character {
         }
     }
     
+    var isAlive: Bool {
+        get {
+            if hp <= 0 {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
     
+    init(startingHP: Int, attackPwr: Int) {
+        self._hp = startingHP
+        self._attackPower = attackPwr
+    }
+    
+    func attemptAttack(attackPwr: Int) -> Bool {
+        self._hp -= attackPwr
+        
+        return true
+    }
 }
---
+++ SimpleRPG/Player.swift
@@ -0,0 +1,30 @@
+//
+//  Player.swift
+//  SimpleRPG
+//
+//  Created by Stanley Pan on 1/27/16.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+import Foundation
+
+class Player: Character {
+    private var _name = "Player"
+    
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    private var _inventory = [String]()
+    
+    var inventory: [String] {
+        return _inventory
+    }
+    
+    convenience init(name: String, hp: Int, attackPwr: Int) {
+        self.init(startingHP: hp, attackPwr: attackPwr)
+        _name = name
+    }
+}
