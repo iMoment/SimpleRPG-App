0a9c27c6bd972dbd7117b7d57af3f1fc97b768c1
Stanley Pan <stanleypan.egre@gmail.com>
2016-01-27 22:52:09 +0900

    Add attack button

--- SimpleRPG/ViewController.swift
+++ SimpleRPG/ViewController.swift
@@ -14,17 +14,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerHpLabel: UILabel!
     @IBOutlet weak var enemyHpLabel: UILabel!
     @IBOutlet weak var enemyImage: UIImageView!
+    @IBOutlet weak var chestButton: UIButton!
+    
+    var player: Player!
+    var enemy: Enemy!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        player = Player(name: "Jarvan", hp: 110, attackPwr: 20)
+        
+        generateRandomEnemy()
+        
+        playerHpLabel.text = "\(player.hp) HP"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func generateRandomEnemy() {
+        let rand = Int(arc4random_uniform(2))
+        if rand == 0 {
+            enemy = Kimara(startingHP: 50, attackPwr: 12)
+        } else {
+            enemy = DevilWizard(startingHP: 60, attackPwr: 15)
+        }
+    }
 
+    @IBAction func onChestTapped(sender: UIButton) {
+    }
 
 }
 
